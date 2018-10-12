c08be872b7338aecd8a33cc77f8da6e82ca093fb
sin <edsinseven@163.com>
2018-10-12 22:18:29 +0800

    抽象工厂模式

---
+++ abstractfactorypattern/AbstractFactory.java
@@ -0,0 +1,8 @@
+package abstractfactorypattern;
+
+import factorypattorn.Shape;
+
+public abstract class AbstractFactory {
+    public abstract Color getColor(String color);
+    public abstract Shape getShape(String shape);
+}
---
+++ abstractfactorypattern/FactoryProducer.java
@@ -0,0 +1,15 @@
+package abstractfactorypattern;
+
+/**
+ * 工厂创造器/生成器类
+ */
+public class FactoryProducer {
+    public static AbstractFactory getFactory(String choice){
+        if(choice.equalsIgnoreCase("SHAPE")){
+            return new ShapeFactory();
+        }else if(choice.equalsIgnoreCase("COLOR")){
+            return new ColorFactory();
+        }
+        return null;
+    }
+}
