caa11251a9b855a31597a832d858d80da745f9ac
sin <edsinseven@163.com>
2018-10-12 22:38:37 +0800

    访问者模式

---
+++ visitorpattern/VisitorPatternDemo.java
@@ -0,0 +1,8 @@
+package visitorpattern;
+
+public class VisitorPatternDemo {
+    public static void main(String[] args) {
+        ComputerPart computer = new Computer();
+        computer.accept(new ComputerPartDisplayVisitor());
+    }
+}
