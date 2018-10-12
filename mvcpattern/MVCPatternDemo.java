e93e86ff6aa1f390d8b0c37ab19bd617fa73e980
sin <edsinseven@163.com>
2018-10-12 22:39:32 +0800

    MVC模式

---
+++ mvcpattern/MVCPatternDemo.java
@@ -0,0 +1,24 @@
+package mvcpattern;
+
+public class MVCPatternDemo {
+    public static void main(String[] args) {
+        //从数据库获取学生数据
+        Student model=retriveStudentFromDatabase();
+        //创建一个视图
+        StudentView view=new StudentView();
+        StudentController controller=new StudentController(model,view);
+
+        controller.updateView();
+        //更新模型数据
+        controller.setStudentName("John");
+        controller.updateView();
+
+    }
+
+    private static Student retriveStudentFromDatabase() {
+        Student student=new Student();
+        student.setName("Robert");
+        student.setRollNo("10");
+        return student;
+    }
+}
